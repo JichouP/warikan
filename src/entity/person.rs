4e7abb60bee992973f3c90fd8540fb783a08c9c3
JichouP <akkunn100@gmail.com>
2025-03-21 12:24:25 +0000

    add entity person

--- src/entity/person.rs
+++ src/entity/person.rs
@@ -1,1 +1,22 @@
+pub struct Person(String);
 
+impl Person {
+    pub fn new(name: String) -> Self {
+        Self(name)
+    }
+
+    pub fn as_inner(&self) -> &String {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_person_new() {
+        let person = Person::new("John".to_string());
+        assert_eq!(person.as_inner(), "John");
+    }
+}
