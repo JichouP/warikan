412edc46e3a01e64fd51273ab703f71581d97f42
JichouP <akkunn100@gmail.com>
2025-03-21 12:12:29 +0000

    add entity money

--- src/entity/money.rs
+++ src/entity/money.rs
@@ -1,1 +1,32 @@
+pub struct Money {
+    pub amount: u32,
+}
 
+impl Money {
+    pub fn new(amount: u32) -> Self {
+        Self { amount }
+    }
+
+    pub fn as_inner(&self) -> u32 {
+        self.amount
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_money_new() {
+        let amount = 100;
+        let money = Money::new(amount);
+        assert_eq!(money.amount, amount);
+    }
+
+    #[test]
+    fn test_money_as_inner() {
+        let amount = 100;
+        let money = Money::new(amount);
+        assert_eq!(money.as_inner(), amount);
+    }
+}
